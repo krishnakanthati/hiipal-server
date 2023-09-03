92bbca53d16c2f9eb45994ec5a6a931c003c58cd
Krishna Kant Hati <krishnakanthati@outlook.com>
2023-09-04 00:55:18 +0530

    cookie

--- functions/index.js
+++ functions/index.js
@@ -87,7 +87,6 @@ function authenticateToken(req, res, next) {
   // const authHeader = req.headers["authorization"];
   // const token = authHeader && authHeader.split(" ")[1];
   const token = String(req.cookies["jwt"]);
-  console.log("tokd", token);
 
   if (token == null) return res.sendStatus(401);
 
